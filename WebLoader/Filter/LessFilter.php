9c2e8d0041dec4a4d9f92323eabcf8efe4cce175
Martin Jantošovič <jantosovic.martin@gmail.com>
2013-05-10 11:49:33 +0200

    Allow absolute path in LESS @import-s

--- WebLoader/Filter/LessFilter.php
+++ WebLoader/Filter/LessFilter.php
@@ -55,7 +55,7 @@ class LessFilter extends \Nette\Object {
 
 		$lessc = $this->getLessC();
 
-		$lessc->importDir = $info['dirname'];
+		$lessc->importDir = [ $info['dirname'], '' ];
 		return $lessc->parse($code);
 	}
 
